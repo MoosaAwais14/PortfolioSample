caf8bc838e87046fafd86254db312bba7a140934
MoosaAwais14 <122404861+MoosaAwais14@users.noreply.github.com>
2024-06-07 08:35:15 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// const typed = document.querySelector('.intro');
+// const text = 'Hi, I\'m Moosa!';
+
+// let i = 0;
+// let speed = 100;
+
+// function typeWriter() {
+//   if (i < text.length) {
+//     typed.textContent += text.charAt(i);
+//     i++;
+//     setTimeout(typeWriter, speed);
+//   }
+// }
+
+// typeWriter();
